b60d3beb952b333b2c462f7484d7e44c3973d31f
dcg <1109923832@qq.com>
2019-01-07 22:43:09 +0800

    all

---
+++ js/baiduhm.js
@@ -0,0 +1,16 @@
+(function() {
+    var hm =   document.createElement("script");
+
+    hm.src =  "https://hm.baidu.com/hm.js?10c7e95763f0d59d76cf123f2a59e112";
+
+    var hm1 =   document.createElement("script");
+
+    hm1.src =  "https://hm.baidu.com/hm.js?cb762fe4c32034d2f8925607cfea9c3e";
+
+
+
+    var s =   document.getElementsByTagName("script")[0];
+
+    s.parentNode.insertBefore(hm, s);
+    s.parentNode.insertBefore(hm1, s);
+})();
